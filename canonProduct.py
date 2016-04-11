6e28c471c1c1ada962ed8edc1ee1022c7561a2f8
pp <pp2moonbird@gmail.com>
2016-04-11 21:49:13 +0800

    extract main

--- canonProduct.py
+++ canonProduct.py
@@ -1,48 +1,55 @@
-productList = []
-
-f = open('product.txt')
-
-lines = f.readlines()
-
-for line in lines:
-    productList.append(line.strip())
-
-for p in productList:
-    print(p)
-
-def generateSearchURL(product):
-    result = product.replace(' ', '+')
-    result = result.replace('/', '%2F')
-    return result
-
-codedProducts = [generateSearchURL(p) for p in productList]
-
-for p in codedProducts:
-    print(p)
-
 import requests
 from bs4 import BeautifulSoup
 
-proxies = {
-  'http': 'http://127.0.0.1:8787',
-  'https': 'http://127.0.0.1:8787'
-}
 
-baseURL = 'https://www.google.com/search?q='
+def getProductListFromFile():
+    productList = []
+    f = open('product.txt')
+    lines = f.readlines()
+    for line in lines:
+        productList.append(line.strip())
+    for p in productList:
+        print(p)
 
-productSearchURL = baseURL + codedProducts[0]
+    def generateSearchURL(product):
+        result = product.replace(' ', '+')
+        result = result.replace('/', '%2F')
+        return result
 
-print(productSearchURL)
+    codedProducts = [generateSearchURL(p) for p in productList]
+    for p in codedProducts:
+        print(p)
 
-r = requests.get(productSearchURL, proxies=proxies)
-
-print(r.text)
-
-with open("googleResult.txt","a+") as f:
-    f.write(r.text)
+    return codedProducts
 
 
 
-soup = BeautifulSoup(r.text)
+def main():
+    print('start')
 
-print(soup.text)
+    codedProducts = getProductListFromFile()
+
+    proxies = {
+        'http': 'http://127.0.0.1:8787',
+        'https': 'http://127.0.0.1:8787'
+    }
+
+    baseURL = 'https://www.google.com/search?q='
+
+    productSearchURL = baseURL + codedProducts[0]
+
+    print(productSearchURL)
+
+    r = requests.get(productSearchURL, proxies=proxies)
+
+    print(r.text)
+
+    with open("googleResult.txt", "a+") as f:
+        f.write(r.text)
+
+    soup = BeautifulSoup(r.text)
+
+    print(soup.text)
+
+if __name__ == '__main__':
+    main()
